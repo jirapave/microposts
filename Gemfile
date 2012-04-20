7a9f52ef65d9880936b20f089da746d25035c38d
jirapave <jirapave@fit.cvut.cz>
2012-04-20 17:48:19 +0200

    Finished layout and routes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.3'
 gem 'execjs'
 gem 'therubyracer'
+gem 'bootstrap-sass', '2.0.0'
 
 group :development do
   gem 'sqlite3', '1.3.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Microposts::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
+  get "users/new"
   
-  get "static_pages/about"
-  
-  get "static_pages/contact"
+  match '/signup',  to: 'users#new'
 
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
