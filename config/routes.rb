1a632a4edbf10ac5a0bf460b9ba77b87eaf71ee2
jirapave <jirapave@fit.cvut.cz>
2012-04-22 16:27:56 +0200

    Make a basic User model with secured passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Microposts::Application.routes.draw do
+  root to: 'static_pages#home'
+  
   get "users/new"
   
   match '/signup',  to: 'users#new'
-
-  root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
