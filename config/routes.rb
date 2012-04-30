eeb08df8b7cd596adb18fa4f5e18819eac93e469
jirapave <jirapave@fit.cvut.cz>
2012-04-30 14:13:30 +0200

    Finish sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Microposts::Application.routes.draw do
   root to: 'static_pages#home'
   
   resources :users
-  
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup',  to: 'users#create'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
